52f0e366dbfab1df33dcdc0300ab3b27a3389b01
Hamed <motealle@gmail.com>
2015-09-15 22:00:04 +0430

    Completed without codiff test

--- info-db-1.py
+++ info-db-1.py
@@ -14,12 +14,30 @@
 # fn Nina
 # *
 import string
-n='b'
-db=[]
-b=0
-a[5][5]=1
-while (n != '*'):
-	n = raw_input('')
-	a[b][1] = n.split()
-	b+=1
-	print a[0]
+n = 'b'
+db = []
+b = 0
+a, c = [], []
+lst = ['no', 'fn', 'ln', 'bd']
+while 1:
+    n = raw_input('')
+    if n == '*':
+        break
+    a.append(n.split())
+    b += 1
+    # print a
+while 1:
+    n = raw_input('')
+    if n == '*':
+        break
+    c = (n.split())
+    # print c
+    if c[0] in lst:
+        # print 'command recogized'
+        for i in range(0, len(a)):
+            if c[1] in a[i]:
+                # print a[i]
+                out = ''
+                for j in range(0, len(a[i])):
+                    out += a[i][j]+' '
+                print out  # len(a[i])
