3c4d288b2ddc3c9868f977f5631b187df9b91fab
Hamed <motealle@gmail.com>
2015-09-17 23:34:15 +0430

    works but not accepted in codiff

---
+++ search-for-nums.py
@@ -0,0 +1,15 @@
+inp = ''
+lst = []
+while True:
+    inp = raw_input()
+    if inp == '0':
+        break
+    if inp.isdigit():
+        lst += inp
+inp = raw_input()
+print ''
+if inp.isdigit():
+    if inp in lst:
+        print 'yes'
+    else:
+        print 'no'
