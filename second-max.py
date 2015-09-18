9593a58836d99230c031a8fb1cd8e8b099c3e76b
Hamed <motealle@gmail.com>
2015-09-19 01:22:12 +0430

    just to store data

--- second-max.py
+++ second-max.py
@@ -1,14 +1,25 @@
-inp = ''
 lst = []
 while True:
-    inp = raw_input()
-    if inp == '0':
-        # lst.append(0)
+    inp = input()
+    if inp == 0:
         break
-    if inp.isdigit():
-        lst.append(int(inp))
+    lst.append(inp)
 if len(lst) >= 2:
     lst.sort()
     lst.reverse()
     print lst[1]
     # print lst
+# inp = ''
+# lst = []
+# while True:
+#     inp = raw_input()
+#     if inp == '0':
+#         # lst.append(0)
+#         break
+#     if inp.isdigit():
+#         lst.append(int(inp))
+# if len(lst) >= 2:
+#     lst.sort()
+#     lst.reverse()
+#     print lst[1]
+#     # print lst
