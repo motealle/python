05dbca97043e76731b2ec72a334592be50a44f62
Hamed <motealle@gmail.com>
2015-09-21 23:06:08 +0330

    tictactoe2.py accepted in codiff

---
+++ tic-tac-toe-2.py
@@ -0,0 +1,60 @@
+import string
+b = []
+
+# a = [[' '][' ']]*3
+# a='b'
+a = []
+for i in range(8):
+    a.append('')
+# print range(8)
+
+
+def tic(a):
+    print '''    1   2   3
+  +---+---+---+
+1 | %s | %s | %s |
+  +---+---+---+
+2 | %s | %s | %s |
+  +---+---+---+
+3 | %s | %s | %s |
+  +---+---+---+''' % ((a[0][0], a[0][1], a[0][2], a[1][0],
+                       a[1][1], a[1][2], a[2][0], a[2][1], a[2][2]))
+
+c = ' '
+d = []
+for i in range(0, 3):
+    b.append([])
+    for j in range(0, 3):
+        b[i].append(' ')
+tic(b)
+status = 'X'
+while True:
+    n = raw_input()
+    if n == '*':
+        break
+    e = ['1', '1']
+    if len(n.split()) == 2:
+        e = (n.split())
+    # print 'e is: ', e
+    # print 'b is: ', b
+    if e[0].isdigit() and e[1].isdigit():
+        # n,m=int(e[0]-1),int(e[1])-1
+
+        if (((status) == 'X') and (int(e[0]) in
+           range(1, 4) and int(e[1]) in range(1, 4)) and
+           b[int(e[0])-1][int(e[1])-1] == ' '):
+            b[int(e[0])-1][int(e[1])-1] = 'X'
+            status = 'O'
+        if (((status) == 'O') and (int(e[0]) in
+           range(1, 4) and int(e[1]) in range(1, 4)) and
+           b[int(e[0])-1][int(e[1])-1] == ' '):
+            b[int(e[0])-1][int(e[1])-1] = 'O'
+            status = 'X'
+    # print 'b is: ', b
+    # print range(1,4)
+    # b[0][0] = 'x'
+    # c = [0*10]
+    # c[int(b[0])][int(b[1])] = b[2]
+    # print c
+
+    tic(b)
