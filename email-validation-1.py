f637e9a843b0cba573128b2bdc2e02fd4c12bc49
Hamed <motealle@gmail.com>
2015-09-29 13:36:06 +0330

    ok but not accepted yet

--- email-validation-1.py
+++ email-validation-1.py
@@ -1,46 +1,48 @@
 import string
 import sys
 domains = {'.com', '.org', '.net', '.COM', '.ORG', '.NET'}
-ills = '~ ! # $ % ^ & * ( ) _ + = - " / \\ | ? > < ` [ ] { } : ' + "'"
+# ills = '~ ! # $ % ^ & * ( ) _ + = - " / \\ | ? > < ` [ ] { } : ' + "'"
+alphas = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ.@'
+als = list(alphas)
 
 
 def check(n):
     # print ills.split()
     # print list(n)
-    isd = []
-    if '@' not in n:
+    isd = ['1']
+    if len(n) < 7:
         isd.append('0')
-        # print "if '@' not in n"
-    if '@' in n:
-        isd.append('1')
-    if n.count('@') != 1:
-        isd.append('0')
-        # print "if n.count('@') != 1:"
-    if n[-4:] not in domains:
-        isd.append('0')
-        # print "if n[-4:] not in domains:"
-    for g in list(n):
-        if g in ills.split():
+    else:
+        for v in list(n):
+            if v.capitalize() not in als:
+                isd.append('0')
+        if n[-4:] not in domains or n[0:1] == '.' or n.count('.') > 2:
             isd.append('0')
-            # print "for g in list(n):if g in ills.split():" ,g
-    if n[0:1] == '.':
-        isd.append('0')
-        # print "if n[0:1] == '.':"
-    if n[n.find('@')-1:n.find('@')] == '.':
-        isd.append('0')
-        # print "if n[n.find('@')-1:n.find('@')] == '.':"
-    if n.count('.') > 2:
-        isd.append('0')
-        # print n.count('.')
-    if not n.find('.', n.find('@')+1) > 1:
-        isd.append('0')
-        # print "if not n.find('.', n.find('@')+1) > 1:"
-    if ((n.split('@')[1]).count('.')) > 1:
-        # print "After @ is %d dots "% ((n.split('@')[1]).count('.'))
-        isd.append('0')
+            # print "if n[-4:] not in domains:"
+        if '@' not in n:
+            isd.append('0')
+            # print "if '@' not in n"
+        else:
+            # isd.append('1')
+            if n.count('@') != 1:
+                isd.append('0')
+            else:
+                if n[n.find('@')-1:n.find('@')] == '.':
+                    isd.append('0')
+                if ((n.split('@')[1]).count('.')) > 1:
+                    # print "After @ is %d dots "%
+                    # ((n.split('@')[1]).count('.'))
+                    isd.append('0')
+                if n[0:1] == '@' or n[-5:-4] == '@':
+                    isd.append('0')
+                if not n.find('.', n.find('@')+1) > 1:
+                    isd.append('0')
+
+            # print "an illegal char is inside: %s" % v
+
     if '0' in isd:
         print '"%s" [no]' % n
-    if '0' not in isd:
+    else:
         print '"%s" [yes]' % n
 # while True:
 #     n = raw_input()
@@ -53,6 +55,6 @@ for line in sys.stdin:
             check(line[:-1])
         else:
             check(line)
-            sys.exit()
-    else:
-        sys.exit()
+    #         sys.exit()
+    # else:
+    #     sys.exit()
