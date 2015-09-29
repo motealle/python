4bb733e114bfea3bd6c3ac6ae36f6d005cb7847f
Hamed <motealle@gmail.com>
2015-09-29 13:35:39 +0330

    accepted in codeiff

--- second-max.py
+++ second-max.py
@@ -3,12 +3,12 @@ while True:
     inp = raw_input()
     if inp == '0':
         break
-    if inp.isdigit():
+    if inp.isdigit() and (int(inp) not in lst):
         lst.append(int(inp))
 if len(lst) >= 2:
     lst.sort()
     lst.reverse()
-    print lst, lst[1]
+    print lst[1]
 
     # print lst
 # inp = ''
