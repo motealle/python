0e5125f39a8ee1a4ebc658ac6567ec022f637a37
Hamed <motealle@gmail.com>
2015-10-07 00:17:53 +0330

    Accepted in Coddiff (String_replace.py)

--- string-replace1.py
+++ string-replace1.py
@@ -3,28 +3,62 @@ import sys
 import re
 
 
+def udigit(isd):
+    for x in (list(isd)):
+        if x in list('-0123456789'):
+            return True
+    else:
+        return False
+
+
+def harfnadad(ish):
+    for x in (list(ish)):
+        if x.upper() in list('ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'):
+            return True
+    else:
+        return False
+
+
+def neg(isneg):
+    if udigit(isneg):
+        if int(isneg) < 0:
+            return True
+        else:
+            return False
+
+
 def repl(n):
     d = n.split()
-    print d
-    if len(d) == 3 and d[1].isdigit() and len(d[2]) == 1:
-        print abs(int(d[1]))
-        if abs(int(d[1])) < len(d[0]):
-            a = list(d[0])
-            a[int(d[1])] = d[2]
-            b = ''.join(a[:])
-            return 'abs < len d[0]',b
+    # print d
+    # print len(d), d[1], udigit(d[1]), len(d[2])
+    if (len(d) == 3 and udigit(d[1]) and
+            len(d[2]) == 1 and harfnadad(d[2])):
+        # print abs(int(d[1]))
+        if not neg(d[1]):
+            if abs(int(d[1])) < len(d[0]):
+                a = list(d[0])
+                a[int(d[1])] = d[2]
+                b = ''.join(a[:])
+                print b
+            else:
+                print ''.join(d[0])
         else:
-            return 'abs not< len d[0]',d[0]
-    elif len(d) == 3 and d[1].isdigit():
-        return 'not accomplished',d[0]
-    else:
-        return 'nothing',n
-
-
+            if abs(int(d[1])) <= len(d[0]):
+                a = list(d[0])
+                a[int(d[1])] = d[2]
+                b = ''.join(a[:])
+                print b
+            else:
+                print ''.join(d[0])
+    # elif len(d) == 3 and udigit(d[1]):
+    #     print ''.join(d[0])
+    # else:
+        # print ''
 
 for line in sys.stdin:
     if line != "\n":
+        # print '\n\nfirst',line.split()
         if line[-1:] == "\n":
-            print repl(line[:-1])
+            repl(line[:-1])
         else:
-            print repl(line)
+            repl(line)
